e2c95ab6db0d9c7b0f94d954b24c151511088294
Toni Kolarić <toni14nexe@gmail.com>
2022-08-09 14:26:55 +0200

    First day finished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
   <div>
-    <img alt="Vue logo" class="logoSvg" src="./assets/logo.svg" />
+    <hr><br><br>
+      <img alt="Vue logo" class="logoSvg" src="./assets/logo.svg" />
+    <br><br><hr><br><br>
+    <center>
+      <button @click="component='reactivity-component'">Async</button>
+
+
+      <br><br><br><hr><br><br>
+      <keep-alive><component v-bind:is="component"/></keep-alive>
+    </center>
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import ReactivityComponent from './components/ReactivityComponent.vue';
+
+export default{
+  name: 'App',
+  components:{
+    'reactivity-component': ReactivityComponent
+  },
+    data(){
+      return{
+          component: 'reactivity-component'
+      }
+    }
+}
 
 </script>
 
---
+++ src/components/ReactivityComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <h1>Reactivity Component</h1>
+        <br>
+        <h3>Counter</h3>
+        <button @click="increment">{{counter}}</button>
+        <br><br>
+        <h3>Cars</h3>
+        <p class="inline">More than 4 types of color: </p>
+        <span class="inline">{{ car.colors.length > 4 ? 'YES' : 'NO' }}</span>
+        <br>
+        <p class="inline">More than 4 types of editions: {{numberOfEditions}}</p>
+        <br><br>
+        <h3 class="inline">Page entered at: </h3>
+        <p class="inline">{{timeNow()}}</p>
+        <br><br>
+        <h3>Get & Set - test</h3>
+        <p>{{getMe('Toni', 'Kolarić')}}</p>
+        <p>{{getMeComputed}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: 0,
+                car: {
+                    name: 'Golf VIII',
+                    colors: [
+                    'Black',
+                    'White',
+                    'Gray',
+                    'Red'
+                    ],
+                    editions: [
+                        'Stock',
+                        'Highline',
+                        'GTD',
+                        'GTI',
+                        'R-line'
+                    ],
+                },
+                name: 'Toni',
+                surname: 'Kolarić',
+                age: 23
+            }
+        },
+        methods: {
+            increment() {
+                this.counter++
+            },
+            timeNow() {
+                const current = new Date();
+                const date = current.getDate()+'.'+(current.getMonth()+1)+'.'+current.getFullYear()+'.';
+                const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+                const dateTime = date +' '+ time;
+                return dateTime;
+            },
+            getMe(name, surname){
+                return name+' '+surname+', '+this.age
+            }
+        },
+        computed:{
+            numberOfEditions(){
+                return this.car.editions.length > 4 ? 'YES' : 'NO';
+            },
+            getMeComputed(){
+                return this.name+' '+this.surname+', '+this.age;
+            }
+        }
+    }
+</script>
+
+<style>
+    .inline{
+        display: inline;
+    }
+</style>
